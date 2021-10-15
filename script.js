bb9bc9a2bb97f3fd6e6f2c0de7f4551ea05ceb76
Nicolò Sangiorgio <n.sangiorgio@hotmail.it>
2021-10-15 16:51:14 +0200

    START: esercizio 2

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 // ESERCIZIO 1
 
-const mioNumero = Math.ceil(Math.random() * 100);
+// const mioNumero = Math.ceil(Math.random() * 100);
 
-console.log("Il tuo numero è: " + mioNumero);
+// console.log("Il tuo numero è: " + mioNumero);
 
-if (mioNumero % 2 === 0) {
-    console.log("Il numero è pari")
-} else {
-    console.log("Il numero è dispari")
-    console.log(mioNumero + 1)
-}
+// if (mioNumero % 2 === 0) {
+//     console.log("Il numero è pari");
+// } else {
+//     console.log("Il numero è dispari");
+//     console.log(mioNumero + 1);
+// }
+
+// ESERCIZIO 2 
+
+const array1 = [15, 2, 7];
+const array2 = [5];
 
 
